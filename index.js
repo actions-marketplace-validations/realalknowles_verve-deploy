2fa1a69b6686cdc796fc2a97c57b3ccb30355bef
alastair <alastair@prism.hr>
2021-03-08 00:32:55 +0000

    Fleshing out the deployment action

---
+++ index.js
@@ -0,0 +1,30 @@
+import AWS from 'aws-sdk'
+import core from '@actions/core'
+
+const accessKeyId = core.getInput('aws-access-key-id')
+const secretAccessKey = core.getInput('aws-secret-access-key')
+const assumeRoleArn = core.getInput('assume-role-arn')
+const functionName = core.getInput('function-name')
+
+const regions = core.getInput('aws-regions').split(',')
+regions.forEach(region => deploy(region).then(() => console.log(`Deployed ${functionName} to ${region}`)))
+
+async function deploy(region) {
+    const sts = new AWS.STS({
+        credentials: {
+            accessKeyId: accessKeyId,
+            secretAccessKey: secretAccessKey
+        },
+        region: region
+    })
+
+    const session = await createSession(sts, region);
+}
+
+async function createSession(sts, region) {
+    return sts.assumeRole({
+        RoleArn: assumeRoleArn,
+        RoleSessionName: `deploy-${functionName}-${region}`,
+        DurationSeconds: 600
+    }).promise()
+}
